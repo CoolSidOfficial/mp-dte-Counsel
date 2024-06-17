c7c400b88fc0c40695cc8f24d5393cdea90d6e71
CoolSidOfficial <hackersiddhant1@gmail.com>
2024-06-17 23:36:04 +0530

    smallchanges

--- src/middle.jsx
+++ src/middle.jsx
@@ -9,11 +9,11 @@ function Middle() {
              <h2 class="text">Counselling Predictor</h2>
             </div>
             <div className="data">
-            
-                <label for="rank"> Rank</label>
-                <input type="number" id="rank" placeholder="Enter your rank" />
+            <form action="http://localhost:3000/submit-form " method="POST">
+                <label for="rank" > Rank</label>
+                <input type="number" id="rank" name="rank"placeholder="Enter your rank" />
                 <label for="category"> Category</label>
-                <select id="category">
+                <select name="category"  id="category">
                     <option>Select Category</option>
                     <option>General</option>
                     <option>OBC</option>
@@ -21,21 +21,22 @@ function Middle() {
                     <option>ST</option>
                     </select> 
                     <label for="round"> Round </label> 
-                    <select id="round">
+                    <select name="round" id ="round">
                     <option> Select Round </option>  
                     <option> Round 1</option>
                     <option> Round 2</option>
                     <option> Round 3</option>
                     </select>
                     <label for="branch"> Branch</label>
-                    <select id="branch">
+                    <select id="branch" name="branch">
                     <option> Select Branch</option>
                         <option> Computer Science</option>
                         <option> Electronics</option>
                         <option> Mechanical</option>
                         <option> Civil</option>
                         </select>
-                        <button>Predict</button>
+                        <input id="button" type="submit" value="Predict"/>
+                        </form>
             </div>
             
     </div>
