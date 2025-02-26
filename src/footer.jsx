cbc39cc5958810e3af349c9021f447b168d6985f
CoolSid <hackersiddhant1@gmail.com>
2025-02-27 03:39:36 +0530

    added tables

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div class="footer">
+    <div className="footer">
       <div className='text'>CoolCounsel. All rights reserved.</div>
       <div className='items'>
       <a href="#"> Privacy Policy </a>
--- src/secondPage.jsx
+++ src/secondPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { Form_Context } from './firstPage'
 import institutes_name from "./final_list"
+import { data } from 'autoprefixer'
 
 
 function SecondPage() {
@@ -9,18 +10,24 @@ function SecondPage() {
     const [ticks, setTicks] = useState([false, false, false, false, false]); 
     const [showTicks, setShowTicks] = useState(false); 
     const [showmark,setshowmark]=useState(false)
+    const [table_data,u_table_data]=useState([])
 
 
+
+    const setdata=(data)=>{
+      u_table_data((current_data)=>[...current_data,data])
+    }
     const handleSelect=(event,index)=>{
       event.preventDefault()
       let matchFound = false; 
+      let temp_key_data;
       for (let key in u_data){
         let simple_string=u_data[key]['INSTITUTE NAME']
         let cleanedStr = simple_string.replace(/\n/g, '');
         //  let cleanedStr=simple_string
-        console.log(cleanedStr,">>",event.target.value)
+        // console.log(cleanedStr,">>",event.target.value)
         if (cleanedStr==event.target.value){
-
+         temp_key_data= u_data[key];
           matchFound = true;
           break;
 
@@ -29,11 +36,13 @@ function SecondPage() {
           console.log("it doesn't match")
         }
       }
+      setdata(temp_key_data)
       const newTicks = [...ticks];
       newTicks[index] = matchFound; 
       setTicks(newTicks);
        
     }
+
      useEffect(()=>{
       const timer=setTimeout(()=>{
         setshowmark(true)
@@ -100,8 +109,46 @@ function SecondPage() {
 
         </select>
         <input type="button" value="Predict" onClick={()=>{setShowTicks(!showTicks)}} />
-</div> 
-   
+
+
+  </div>
+{showTicks &&
+
+<div className='bg-gray-800 w-full p-5'>
+  <div className=' text-white text-center text-4xl p-5 font-mono font-semibold'>MPDTE Counselling Allotment Status(2023) </div>
+  <table className='bg-gray-700 mx-auto text-3xl border w-full  rounded-md text-white font-serif '>
+    <tr className='border border-gray-400 px-4 py-2'>
+      <th>Preference</th>
+      <th>College Name</th>
+      <th>Opening Rank</th>
+      <th>Closing Rank</th>
+
+      <th>Allotment Status</th>
+    </tr>
+<tbody className=''>
+  {table_data?.length > 0 ? (
+    table_data
+      .filter((data) => data) // Remove undefined/null values
+      .map((data, index) => (
+        <tr key={data?.id || index} className='border border-gray-400 px-6 py-2'>
+          <td>{index + 1}</td>
+          <td>{data?.["INSTITUTE NAME"] ?? "N/A"}</td>
+          <td>{data?.["JEE OPENING RANK"] ?? "N/A"}</td>
+          <td>{data?.["JEE CLOSING RANK"] ?? "N/A"}</td>
+          <td>☑️ Allotable ☑️</td>
+        </tr>
+      ))
+  ) : (
+    <tr>
+      <td colSpan="5">No data available</td>
+    </tr>
+  )}
+</tbody>
+
+
+  </table>
+  </div> 
+}
     </>
   )
 }
